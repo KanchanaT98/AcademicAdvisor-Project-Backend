19f5aa2c3457eb8a11db7c8f3c932ace8d923a40
KanchanaT98 <thennakoon.kanchana@gmail.com>
2024-11-29 09:28:47 +0530

    initialized a spring project

---
+++ src/main/java/com/project/AcademicAdvisor/AcademicAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.project.AcademicAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/AcademicAdvisor/AcademicAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.AcademicAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
